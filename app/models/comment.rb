641375cb05918cc9026dcd831fa9074364e0501a
Vladislav <lvo123ua@gmail.com>
2018-07-02 13:49:44 +0300

    bug fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
   validates :commenter, presence: true
-  validates :body, presence: true, length: { maximum: 65 }
+  validates :body, presence: true
 end
