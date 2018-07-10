b3e09420a15e147b60377104ddab84e1e1e38a43
Vladislav <lvo123ua@gmail.com>
2018-07-10 14:41:26 +0300

    Update bd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_081258) do
+ActiveRecord::Schema.define(version: 2018_07_10_082652) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
