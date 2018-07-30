47dffbdffc4b8a634e137505cc4c4a19cd5095d1
Vladislav <lvo123ua@gmail.com>
2018-07-30 21:38:47 +0300

    Bug fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy, inverse_of: :article
   belongs_to :user
   has_many :filters
-  has_many :categories, through: :filters
+  has_many :categories,dependent: :destroy, through: :filters
 
   validates_associated :comments
   validates :title, presence: true, length: { minimum: 5 }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
   validates :body, presence: true
+
+  after_save :get_user_name
+  after_save :get_user_id
+  
+  private
+
+  def get_user_id
+    self.user_id = user.id
+  end
+
+  def get_user_name
+    self.commenter = user.user_name
+  end
 end
